e386fb80265edc3d00ac8639adf41bdc538438c8
Ezgi Senyurt-Ertürk <156966522+ezgsnyrt@users.noreply.github.com>
2024-05-22 14:13:38 +0200

    add icons and provide its link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { FaHandRock, FaHandPaper, FaHandScissors } from "react-icons/fa";
 
 function App() {
   return (
@@ -8,18 +9,18 @@ function App() {
       <h1>Rock, Paper, Scissors Game!</h1>
         <div className="score-board">
           <div className="player">
-            <div>You: 0</div>
+            <div className="score">You: 0</div>
             <div></div>
           </div>
           <div className="player">
-            <div>Computer: 0</div>
+            <div className="score">Computer: 0</div>
             <div></div>
           </div>
         </div>
         <div>
-          <button className="move-btn">Rock</button>
-          <button className="move-btn">Paper</button>
-          <button className="move-btn">Scissors</button>
+          <button className="move-btn"><FaHandRock /></button>
+          <button className="move-btn"><FaHandPaper /></button>
+          <button className="move-btn"><FaHandScissors /></button>
         </div>
         <h2>You win!</h2>
       </div>
