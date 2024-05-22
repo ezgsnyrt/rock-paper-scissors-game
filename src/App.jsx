578be1a8a1c803e4c42829370d61a523e022e0f5
Ezgi Senyurt-Ertürk <156966522+ezgsnyrt@users.noreply.github.com>
2024-05-22 13:23:27 +0200

    add initial structure for the game

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <p>Rock, Paper, Scissors Game!</p>
+      <div>
+        <div>
+          <div>
+            <div>You: 0</div>
+            <div></div>
+          </div>
+          <div>
+            <div>Computer: 0</div>
+            <div></div>
+          </div>
+        </div>
+        <div>
+          <button>Rock</button>
+          <button>Paper</button>
+          <button>Scissors</button>
+        </div>
+        <h2>You win!</h2>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
